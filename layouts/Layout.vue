c404693b56fb5b740082cd70fae67d9003e2251b
Alex Bea <abea@mailbox.org>
2021-11-29 15:15:27 -0600

    Adds option to add sidebar A3 highlight

---
+++ layouts/Layout.vue
@@ -0,0 +1,33 @@
+<template>
+  <ParentLayout>
+    <template #sidebar-bottom>
+      <section v-if="$themeConfig && $themeConfig.latestVersionPromo"
+        class="layout-note"
+      >
+        <p>
+          <a href="https://v3.docs.apostrophecms.org/">Learn about Apostrophe 3. 🆕</a>
+        </p>
+      </section>
+    </template>
+  </ParentLayout>
+</template>
+
+<script>
+import ParentLayout from '@parent-theme/layouts/Layout.vue'
+
+export default {
+  name: 'Layout',
+  components: {
+    ParentLayout
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .layout-note
+    background-color: $badgeTipColor
+    padding 0.35rem 1rem 0.35rem 1.25rem
+
+    a
+      color: white
+</style>
